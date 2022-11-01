e47446139663d4543d114f8272682cddb4619912
Alexandra Chernova <chernova.aleksandra28@gmail.com>
2022-11-01 15:03:52 +0300

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double bmi(double height, int weight) {
         double index;
-        index = weight / height /height;
+        index = weight / height / height;
         return index;
     }
 }
